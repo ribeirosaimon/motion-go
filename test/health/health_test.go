7f1ba3bb93e35fa1f0091af465fbea88b4742dc2
Saimon <saimonribeiros@hotmail.com>
2023-02-21 21:29:20 -0300

    create beanchmark test

--- test/health/health_test.go
+++ test/health/health_test.go
@@ -6,35 +6,43 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/gin-gonic/gin"
 	"github.com/ribeirosaimon/motion-go/pkg/health"
+	"github.com/ribeirosaimon/motion-go/test/util"
 )
 
-func TestGetUser(t *testing.T) {
-	// Cria um router do Gin vazio
-	r := gin.New()
-
-	health.NewHeathController(r)
-
-	req, err := http.NewRequest(http.MethodGet, "/health", nil)
-	if err != nil {
-		t.Fatal(err)
+func BenchmarkController(b *testing.B) {
+	resp, req, err := util.CreateEngineRequest(http.MethodGet, "/health", nil)
+	if resp.Code != http.StatusOK {
+		b.Errorf("Expected Http status: %d; but is received: %d", http.StatusOK, resp.Code)
 	}
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-	if w.Code != http.StatusOK {
-		t.Errorf("Expected Http status: %d; but is received: %d", http.StatusOK, w.Code)
+	if err != nil {
+		b.Error("error in request")
+	}
+	for i := 0; i < b.N; i++ {
+		// Grava a resposta HTTP
+		response := httptest.NewRecorder()
+
+		// Processa a solicitação HTTP usando o roteador do Gin
+		util.GetEnginer().ServeHTTP(response, req)
+	}
+}
+
+func TestController(t *testing.T) {
+	resp, _, err := util.CreateEngineRequest(http.MethodGet, "/health", nil)
+
+	if resp.Code != http.StatusOK {
+		t.Errorf("Expected Http status: %d; but is received: %d", http.StatusOK, resp.Code)
 	}
 
 	var response = health.NewHealthApiResponse()
-	err = json.Unmarshal(w.Body.Bytes(), &response)
+	err = json.Unmarshal(resp.Body.Bytes(), &response)
 	if err != nil {
 		t.Errorf("Unmarshal erro: %s", err.Error())
 	}
 	if response.Ready != true {
-		t.Errorf("Status must be bool: %s", w.Body.String())
+		t.Errorf("Status must be bool: %s", resp.Body.String())
 	}
 	if &response.Time == nil {
-		t.Errorf("Time must be unlike nil: %s", w.Body.String())
+		t.Errorf("Time must be unlike nil: %s", resp.Body.String())
 	}
 }
